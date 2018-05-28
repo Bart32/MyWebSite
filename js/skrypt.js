325a6740270dde10e75debe65189d5de6d935ca3
Bart32 <bartosz.kosiec@gmail.com>
2018-05-28 18:20:56 +0200

    Almost done
    The page is not responsive yet

--- js/skrypt.js
+++ js/skrypt.js
@@ -2,59 +2,126 @@
 window.onload = function(){
 
 
-// Back to top and leftIcons
+// Showing button, left icons and html, css, js images
 
-var btt = document.getElementById('backToTop');
-var leftIcon = document.getElementById('LeftIcons');
-
-btt.style.display = 'none';
-leftIcon.style.display = 'none';
+$('.hiddenClass').hide();
 
 window.onscroll = function(){
+  var testy = document.getElementById('testy');
+  testy.innerHTML = window.pageYOffset;
+  if(window.pageYOffset > 320){
+    $('#jspng').slideDown();
+    $('#htmlpng').slideDown();
+    $('#csspng').slideDown();
+
+  }
   if(window.pageYOffset > 350){
-    btt.style.display = 'block';
-    leftIcon.style.display = 'block';
+    $('#backToTop').show();
+    $('#LeftIcons').show();
   }
   else{
-    btt.style.display = 'none';
-    leftIcon.style.display = 'none';
+    $('#backToTop').hide();
+    $('#LeftIcons').hide();
+  }
+
+  if(window.pageYOffset > 575){
+  $('#responsivepng').slideDown();
+  $('#bootstrappng').slideDown();
+  $('#jquerypng').slideDown();
+  }
+
+  if(window.pageYOffset > 820){
+  $('#gitpng').slideDown();
+  $('#joomlapng').slideDown();
+  $('#mysqlpng').slideDown();
+  }
+
+  if(window.pageYOffset > 1254){
+  $('#debon').slideDown();
+  $('#template').slideDown();
+  $('#bartek').slideDown();
   }
 }
+
 
 $('#backToTop').click(function () {
    $('html, body').animate({scrollTop: 0}, 900);
 });
 
-// Hidden HTML/CSS/JS elements
-
-  $('#jsimg').hover(function(){
-    $('#jqueryimg').slideDown();
-        $(this).css('opacity', '1');
-  });
-  $('#htmlimg').hover(function(){
-    $('#responsiveimg').slideDown();
-    $(this).css('opacity', '1');
-  });
-  $('#cssimg').hover(function(){
-    $('#bootstrapimg').slideDown();
-    $(this).css('opacity', '1');
-  });
-
 // Menu
 
 $('#aboutMenu').click(function (){
-  $('html, body').animate({ scrollTop: $('#about').offset().top }, 1000);
+  $('html, body').animate({ scrollTop: $('#about').offset().top }, 1200);
 });
 $('#projectsMenu').click(function (){
-  $('html, body').animate({ scrollTop: $('#projects').offset().top }, 1000);
+  $('html, body').animate({scrollTop: '+=1596px'}, 1200);
 });
 $('#contactMenu').click(function (){
-  $('html, body').animate({ scrollTop: $('#contact').offset().top }, 1000);
+  $('html, body').animate({scrollTop: '+=2238px'}, 1200);
 });
 
-// Left icon's
-$('#leftIcons')
+// Projects
 
 
+
+// Project name and Project technologies
+$('.ProjectTechnologies').hide();
+$('.projectName').hide();
+
+$('.firstProject').mouseover(function (){
+    $('#debonName').show( "fast", function() {
+  });
+    $('#debonTechnologies').show( "fast", function() {
+  });
+  }).mouseout (function () {
+      $('#debonName').hide( "fast", function() {
+    });
+      $('#debonTechnologies').hide( "fast", function() {
+    });
+  });
+
+  $('.secondProject').mouseover(function (){
+      $('#templateName').show( "fast", function() {
+    });
+      $('#templateTechnologies').show( "fast", function() {
+    });
+    }).mouseout (function () {
+        $('#templateName').hide( "fast", function() {
+      });
+        $('#templateTechnologies').hide( "fast", function() {
+      });
+    });
+
+$('.thirdProject').mouseover(function (){
+    $('#kosiecName').show( "fast", function() {
+  });
+    $('#kosiecTechnologies').show( "fast", function() {
+  });
+  }).mouseout (function () {
+      $('#kosiecName').hide( "fast", function() {
+    });
+      $('#kosiecTechnologies').hide( "fast", function() {
+    });
+  });
+
+// Projects image change
+
+$(".firstProject").mouseover(function () {
+  $('#debon').attr("src","images/projects/debon2Small.png");
+}).mouseout(function () {
+  $('#debon').attr("src","images/projects/debonSmall.png");
+});
+
+$(".secondProject").mouseover(function () {
+  $('#template').attr("src","images/projects/template2Small.png");
+}).mouseout(function () {
+  $('#template').attr("src","images/projects/templateSmall.png");
+});
+
+$(".thirdProject").mouseover(function () {
+  $('#bartek').attr("src","images/projects/bartek2Small.png");
+}).mouseout(function () {
+  $('#bartek').attr("src","images/projects/bartekSmall.png");
+});
 
 }
